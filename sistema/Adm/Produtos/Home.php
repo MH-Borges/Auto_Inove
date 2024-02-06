64c2895d11b613ed5aeb21419a5ea2fa7e462399
Matheus Henrique <matheush.borges@hotmail.com>
2024-02-06 18:28:45 -0300

    Telas de relatorio e produtos
    tela base de relatórios mais a tela inicial da parte de produtos iniciada

--- sistema/Adm/Produtos/Home.php
+++ sistema/Adm/Produtos/Home.php
@@ -1,4 +1,123 @@
-<div class="blockContent HomeAdm">
+<div class="blockContent produtosHome">
 
+    <div class="produtos_topSells">
+        <h2>Top 10 mais vendidos</h2>
+        <div class="topSell_Header">
+            <h3>Produto</h3>
+            <h4>Valores</h4>
+        </div>
+        <div class="topSell_Itens">
+            <div class="topSell_Item">
+                <div class="topSell_Produto">
+                    <img src="../../assets/icons/Placeholder.svg" onload="SVGInject(this)">
+                    <p>Lorem Ipsum Dolon</p>
+                </div>
+                <div class="topSell_Valores">
+                    <p><span>R$</span>99.999,00</p>
+                    <div class="status Ativo">Ativo</div>
+                </div>
+            </div>
+            <div class="topSell_Item">
+                <div class="topSell_Produto">
+                    <img src="../../assets/icons/Placeholder.svg" onload="SVGInject(this)">
+                    <p>Lorem Ipsum Dolon</p>
+                </div>
+                <div class="topSell_Valores">
+                    <p><span>R$</span>99.999,00</p>
+                    <div class="status semEstoque">Sem estoque</div>
+                </div>
+            </div>
+            <div class="topSell_Item">
+                <div class="topSell_Produto">
+                    <img src="../../assets/icons/Placeholder.svg" onload="SVGInject(this)">
+                    <p>Lorem Ipsum Dolon</p>
+                </div>
+                <div class="topSell_Valores">
+                    <p><span>R$</span>99.999,00</p>
+                    <div class="status Ativo">Ativo</div>
+                </div>
+            </div>
+            <div class="topSell_Item">
+                <div class="topSell_Produto">
+                    <img src="../../assets/icons/Placeholder.svg" onload="SVGInject(this)">
+                    <p>Lorem Ipsum Dolon</p>
+                </div>
+                <div class="topSell_Valores">
+                    <p><span>R$</span>99.999,00</p>
+                    <div class="status Ativo">Ativo</div>
+                </div>
+            </div>
+            <div class="topSell_Item">
+                <div class="topSell_Produto">
+                    <img src="../../assets/icons/Placeholder.svg" onload="SVGInject(this)">
+                    <p>Lorem Ipsum Dolon</p>
+                </div>
+                <div class="topSell_Valores">
+                    <p><span>R$</span>99.999,00</p>
+                    <div class="status Inativo">Desativado</div>
+                </div>
+            </div>
+            <div class="topSell_Item">
+                <div class="topSell_Produto">
+                    <img src="../../assets/icons/Placeholder.svg" onload="SVGInject(this)">
+                    <p>Lorem Ipsum Dolon</p>
+                </div>
+                <div class="topSell_Valores">
+                    <p><span>R$</span>99.999,00</p>
+                    <div class="status Ativo">Ativo</div>
+                </div>
+            </div>
+            <div class="topSell_Item">
+                <div class="topSell_Produto">
+                    <img src="../../assets/icons/Placeholder.svg" onload="SVGInject(this)">
+                    <p>Lorem Ipsum Dolon</p>
+                </div>
+                <div class="topSell_Valores">
+                    <p><span>R$</span>99.999,00</p>
+                    <div class="status semEstoque">Sem estoque</div>
+                </div>
+            </div>
+            <div class="topSell_Item">
+                <div class="topSell_Produto">
+                    <img src="../../assets/icons/Placeholder.svg" onload="SVGInject(this)">
+                    <p>Lorem Ipsum Dolon</p>
+                </div>
+                <div class="topSell_Valores">
+                    <p><span>R$</span>99.999,00</p>
+                    <div class="status Inativo">Desativado</div>
+                </div>
+            </div>
+            <div class="topSell_Item">
+                <div class="topSell_Produto">
+                    <img src="../../assets/icons/Placeholder.svg" onload="SVGInject(this)">
+                    <p>Lorem Ipsum Dolon lorem ate asfdadf affsdfasdfasdfsad sdf asdfasdf asdf  lorem fsdf sfadsfasd  fdasfsd asdfasd fadf</p>
+                </div>
+                <div class="topSell_Valores">
+                    <p><span>R$</span>99.999,00</p>
+                    <div class="status Ativo">Ativo</div>
+                </div>
+            </div>
+            <div class="topSell_Item">
+                <div class="topSell_Produto">
+                    <img src="../../assets/icons/Placeholder.svg" onload="SVGInject(this)">
+                    <p>Lorem Ipsum Dolon</p>
+                </div>
+                <div class="topSell_Valores">
+                    <p><span>R$</span>99.999,00</p>
+                    <div class="status Ativo">Ativo</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="produtos_SemEstoque">
+
+    </div>
+    <div class="produtos_UltimosVendidos">
+
+    </div>
+    <div class="produtos_Grafico">
+        <h5>Em Breve...</h5>
+    </div>
 
 </div>
--- sistema/Adm/Relatorios/Home.php
+++ sistema/Adm/Relatorios/Home.php
@@ -1,4 +1,33 @@
-<div class="blockContent HomeAdm">
+<section class="blockContent relatoriosHome">
 
+    <a class="relatorio_Links" href="#">
+        <img src="../../assets/icons/vendas_Gerais.svg" onload="SVGInject(this)">
+        <h3>Vendas gerais</h3>
+    </a>
 
-</div>
+    <a class="relatorio_Links" href="#">
+        <img src="../../assets/icons/mais_Vendidos.svg" onload="SVGInject(this)">
+        <h3>Mais vendidos</h3>
+    </a>
+
+    <a class="relatorio_Links" href="#">
+        <img src="../../assets/icons/ultimos_Vendidos.svg" onload="SVGInject(this)">
+        <h3>Últimos vendidos</h3>
+    </a>
+
+    <a class="relatorio_Links" href="#">
+        <img src="../../assets/icons/produtos_Sem_Estoque.svg" onload="SVGInject(this)">
+        <h3>Produtos <br> sem estoque</h3>
+    </a>
+
+    <a class="relatorio_Links" href="#">
+        <img src="../../assets/icons/ultimas_Atualizacoes.svg" onload="SVGInject(this)">
+        <h3>Ultimas atualizações</h3>
+    </a>
+
+    <a class="relatorio_Links" href="#">
+        <img src="../../assets/icons/atualizacoes_Paginas_Vendas.svg" onload="SVGInject(this)">
+        <h3>Atualizações <br> página de vendas</h3>
+    </a>
+
+</section>
