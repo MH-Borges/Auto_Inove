3d8d3d2207ed3809910e7042969a842061d498f3
Matheus Henrique <matheush.borges@hotmail.com>
2024-02-07 22:54:23 -0300

    Tela inicial produtos

--- sistema/Adm/Produtos/Home.php
+++ sistema/Adm/Produtos/Home.php
@@ -2,11 +2,11 @@
 
     <div class="produtos_topSells">
         <h2>Top 10 mais vendidos</h2>
-        <div class="topSell_Header">
+        <div class="produtos_TopSell_Header">
             <h3>Produto</h3>
             <h4>Valores</h4>
         </div>
-        <div class="topSell_Itens">
+        <div class="produtos_TopSell_Itens">
             <div class="topSell_Item">
                 <div class="topSell_Produto">
                     <img src="../../assets/icons/Placeholder.svg" onload="SVGInject(this)">
@@ -111,13 +111,139 @@
     </div>
 
     <div class="produtos_SemEstoque">
-
+        <h2>Produtos sem estoque</h2>
+        <div class="Itens">
+            <div class="Item">
+                <div class="Produto">
+                    <img src="../../assets/icons/Placeholder.svg" onload="SVGInject(this)">
+                    <p>Lorem Ipsum Dolon</p>
+                </div>
+                <div class="Valores">
+                    <p><span>R$</span>99.999,00</p>
+                    <div class="status semEstoque">Ativo</div>
+                </div>
+            </div>
+            <div class="Item">
+                <div class="Produto">
+                    <img src="../../assets/icons/Placeholder.svg" onload="SVGInject(this)">
+                    <p>Lorem Ipsum Dolon</p>
+                </div>
+                <div class="Valores">
+                    <p><span>R$</span>99.999,00</p>
+                    <div class="status semEstoque">Sem estoque</div>
+                </div>
+            </div>
+            <div class="Item">
+                <div class="Produto">
+                    <img src="../../assets/icons/Placeholder.svg" onload="SVGInject(this)">
+                    <p>Lorem Ipsum Dolon</p>
+                </div>
+                <div class="Valores">
+                    <p><span>R$</span>99.999,00</p>
+                    <div class="status semEstoque">Ativo</div>
+                </div>
+            </div>
+            <div class="Item">
+                <div class="Produto">
+                    <img src="../../assets/icons/Placeholder.svg" onload="SVGInject(this)">
+                    <p>Lorem Ipsum Dolon</p>
+                </div>
+                <div class="Valores">
+                    <p><span>R$</span>99.999,00</p>
+                    <div class="status semEstoque">Ativo</div>
+                </div>
+            </div>
+        </div>
+        <a href="#">Veja mais...</a>
     </div>
+
     <div class="produtos_UltimosVendidos">
-
+        <h2>Últimos vendidos</h2>
+        <div class="Itens">
+            <div class="Item">
+                <div class="Produto">
+                    <img src="../../assets/icons/Placeholder.svg" onload="SVGInject(this)">
+                    <p>Lorem Ipsum Dolon</p>
+                </div>
+                <div class="Valores">
+                    <p><span>R$</span>99.999,00</p>
+                </div>
+            </div>
+            <div class="Item">
+                <div class="Produto">
+                    <img src="../../assets/icons/Placeholder.svg" onload="SVGInject(this)">
+                    <p>Lorem Ipsum Dolon</p>
+                </div>
+                <div class="Valores">
+                    <p><span>R$</span>99.999,00</p>
+                </div>
+            </div>
+            <div class="Item">
+                <div class="Produto">
+                    <img src="../../assets/icons/Placeholder.svg" onload="SVGInject(this)">
+                    <p>Lorem Ipsum Dolon</p>
+                </div>
+                <div class="Valores">
+                    <p><span>R$</span>99.999,00</p>
+                </div>
+            </div>
+            <div class="Item">
+                <div class="Produto">
+                    <img src="../../assets/icons/Placeholder.svg" onload="SVGInject(this)">
+                    <p>Lorem Ipsum Dolon</p>
+                </div>
+                <div class="Valores">
+                    <p><span>R$</span>99.999,00</p>
+                </div>
+            </div>
+        </div>
+        <a href="#">Veja mais...</a>
     </div>
+
     <div class="produtos_Grafico">
         <h5>Em Breve...</h5>
+    </div>
+
+    <div class="produtos_Btn_Criacao">
+        <button type="button" data-toggle="dropdown" aria-expanded="false">
+            <p>Criar</p>
+            <img src="../../assets/icons/add.svg">
+        </button>
+        <div class="dropdown-menu">
+            <button class="dropdown-item" type="button" data-toggle="modal" data-target="#ModalLogout">
+                <p>Nova categoria</p>
+            </button>
+            <button class="dropdown-item" type="button" data-toggle="modal" data-target="#ModalLogout">
+                <p>Nova sub-categoria</p>
+            </button>
+            <button class="dropdown-item" type="button" data-toggle="modal" data-target="#ModalLogout">
+                <p>Novo produto</p>
+            </button>
+            <button class="dropdown-item" type="button" data-toggle="modal" data-target="#ModalLogout">
+                <p>Novo código</p>
+            </button>
+        </div>
+    </div>
+
+    <div class="produtos_Btn_Listagens">
+        <button type="button" data-toggle="dropdown" aria-expanded="false">
+            <p>Listagens</p>
+            <img src="../../assets/icons/list.svg">
+        </button>
+        <div class="dropdown-menu">
+            <button class="dropdown-item" type="button" data-toggle="modal" data-target="#ModalLogout">
+                <p>Lista de categorias</p>
+            </button>
+            <button class="dropdown-item" type="button" data-toggle="modal" data-target="#ModalLogout">
+                <p>Lista de sub-categorias</p>
+            </button>
+            <button class="dropdown-item" type="button" data-toggle="modal" data-target="#ModalLogout">
+                <p>Lista de produtos</p>
+            </button>
+            <button class="dropdown-item" type="button" data-toggle="modal" data-target="#ModalLogout">
+                <p>Lista de códigos</p>
+            </button>
+        </div>
     </div>
 
 </div>
